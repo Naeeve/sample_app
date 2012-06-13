3b8c59353214157789845ded27dd60412c325b4c
Naeeve <engr_naeemullah@hotmail.com>
2012-06-13 10:24:27 -0700

    controller destroyed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
