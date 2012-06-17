9ce73ad5cf00a0f947d357aaea5280569b9622d0
Naeeve <engr_naeemullah@hotmail.com>
2012-06-17 00:38:15 -0700

    Putting Commit before starting layouting

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,11 +8,15 @@ it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-it "should have the title 'Home'" do
+it "should have the base title" do
 	  visit '/static_pages/home'
 	  page.should have_selector('title',
-	  					:text => "Ruby on Rails Tutorial Sample App | Home")
+	  					:text => "Ruby on Rails Tutorial Sample App")
 	end
+#  it "should not have a custome page title" do
+ #   visit '/static_pages/home'
+  #  page.should_not have_selector('title', :text => '|Home')
+  #end
   end
 
 describe "Help page" do
@@ -22,11 +26,15 @@ it "should have the h1 'Help'" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-it "should have the title 'Help'" do
+it "should have the base title" do
 	visit '/static_pages/help'
 	page.should have_selector('title',
-	  					:text => "Ruby on Rails Tutorial Sample App | Help")
+	  					:text => "Ruby on Rails Tutorial Sample App")
 	end
+  it "should not have a custome page title" do
+    visit '/static_pages/help'
+    page.should_not have_selector('title', :text => '|Help')
+  end
   end
 
   describe "About Us page" do
@@ -36,11 +44,15 @@ it "should have the h1 'About Us'" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-it "should have the title 'About Us'" do
+it "should have the base title" do
 	visit '/static_pages/about'
 	page.should have_selector('title',
-						:text => "Ruby on Rails Tutorial Sample App | About Us")
+						:text => "Ruby on Rails Tutorial Sample App")
 	end
+  it "should not have a custome page title" do
+    visit '/static_pages/about'
+    page.should_not have_selector('title', :text => '| About Us')
+  end
   end
 
 end
